108b2468cd683cc3d24ac387b232648cf3f9c8d9
David-Retina15 <david77115@gmail.com>
2018-12-09 21:31:51 +0800

    feat: import biometric to support fingerprint api

--- app/src/main/java/com/beibeilab/keepin/frontpage/MainFragment.kt
+++ app/src/main/java/com/beibeilab/keepin/frontpage/MainFragment.kt
@@ -1,12 +1,17 @@
 package com.beibeilab.keepin.frontpage
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.hardware.biometrics.BiometricPrompt
 import androidx.fragment.app.Fragment
 import android.os.Bundle
+import android.os.CancellationSignal
 import androidx.recyclerview.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
 import com.beibeilab.keepin.MainActivity
 import com.beibeilab.keepin.R
 import com.beibeilab.keepin.account.AccountFragment
@@ -22,6 +27,8 @@ import kotlinx.android.synthetic.main.fragment_main.*
 class MainFragment : Fragment(), MainAdapter.OnItemClickListner {
 
     private lateinit var viewModel: MainViewModel
+    private var fingerprintDialog: AlertDialog? = null
+    private var selectedAccount: AccountEntity? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,7 +58,9 @@ class MainFragment : Fragment(), MainAdapter.OnItemClickListner {
     }
 
     override fun itemOnClicked(position: Int, account: AccountEntity) {
-        (activity as MainActivity).replaceFragment(R.id.fragment_content, AccountFragment.newInstance(account))
+        selectedAccount = account
+
+        showBiometricPromt()
     }
 
     private fun setupViewModel() {
@@ -64,14 +73,36 @@ class MainFragment : Fragment(), MainAdapter.OnItemClickListner {
                 onItemClickListner = this@MainFragment
             }
             layoutManager = LinearLayoutManager(
-                context!!,
-                LinearLayoutManager.VERTICAL,
-                false
-            )
+                context!!, RecyclerView.VERTICAL, false)
         }
     }
 
     private fun populateList(list: List<AccountEntity>) {
         (recyclerView.adapter as MainAdapter).items = list
+    }
+
+    private fun showBiometricPromt(){
+        val executor = context!!.mainExecutor
+        val cancelListener = DialogInterface.OnClickListener { _, _ ->
+
+        }
+
+        resources.getString(R.string.fingerprint_title)
+
+        val biometricPrompt = BiometricPrompt.Builder(context!!)
+            .setTitle(resources.getString(R.string.fingerprint_title))
+            .setSubtitle(resources.getString(R.string.fingerprint_subtitle))
+            .setNegativeButton(resources.getString(R.string.cancel), executor, cancelListener)
+            .build()
+
+        biometricPrompt.authenticate(CancellationSignal(), executor, object:BiometricPrompt.AuthenticationCallback(){
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+                jumpt2AccountFragment()
+            }
+        })
+    }
+
+    private fun jumpt2AccountFragment(){
+        (activity as MainActivity).replaceFragment(R.id.fragment_content, AccountFragment.newInstance(selectedAccount!!))
     }
 }
