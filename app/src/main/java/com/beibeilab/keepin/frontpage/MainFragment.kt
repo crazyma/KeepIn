8c7f84096ef627ec25087489a3fcf43024b1940d
David-Retina15 <david77115@gmail.com>
2019-08-09 19:56:59 +0800

    feat: create UIKits module
    first component: AlertDialogFragment

--- app/src/main/java/com/beibeilab/keepin/frontpage/MainFragment.kt
+++ app/src/main/java/com/beibeilab/keepin/frontpage/MainFragment.kt
@@ -2,10 +2,9 @@ package com.beibeilab.keepin.frontpage
 
 import androidx.fragment.app.Fragment
 import android.os.Bundle
+import android.util.Log
+import android.view.*
 import androidx.recyclerview.widget.LinearLayoutManager
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
 import com.beibeilab.keepin.MainActivity
@@ -15,12 +14,13 @@ import com.beibeilab.keepin.database.AccountDatabase
 import com.beibeilab.keepin.database.AccountEntity
 import com.beibeilab.keepin.extension.obtainViewModel
 import com.beibeilab.keepin.extension.replaceFragment
+import com.beibeilab.uikits.alertdialog.AlertDialogFragment
 import kotlinx.android.synthetic.main.fragment_main.*
 
 /**
  * A placeholder fragment containing a simple view.
  */
-class MainFragment : Fragment(), MainAdapter.OnItemClickListener {
+class MainFragment : Fragment(), MainAdapter.OnItemClickListener, AlertDialogFragment.Callback {
 
     private lateinit var viewModel: MainViewModel
     private var mainAdapter: MainAdapter? = null
@@ -50,6 +50,36 @@ class MainFragment : Fragment(), MainAdapter.OnItemClickListener {
     override fun onResume() {
         super.onResume()
         (activity as MainActivity).showFAB()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_main, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_backup -> {
+                AlertDialogFragment.Builder(context!!)
+                    .setTitle("title")
+                    .setMessage("message")
+                    .setPositiveButton("ok")
+                    .show(childFragmentManager, "backup")
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onDialogAction(fragment: AlertDialogFragment, action: Int, extras: Bundle?) {
+        when (fragment.tag) {
+            "backup" -> {
+                when (action) {
+                    AlertDialogFragment.ACTION_POSITIVE -> {
+                        Log.d("badu", "XDD")
+                    }
+                }
+            }
+        }
     }
 
     override fun itemOnClicked(position: Int, account: AccountEntity) {
